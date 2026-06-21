d3efb4bf7b15d4c71fd80d7288a199344844223c
Saif Iqbal <112165961+SaifIqbal2@users.noreply.github.com>
2026-06-22 04:57:39 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,8 @@ function App() {
             <Route path="/blog/:id" element={<Post />} />
           </Routes>
         </main>
+<script async="async" data-cfasync="false" src="https://pl29836618.effectivecpmnetwork.com/f1bd6d29bbaabfc89aa7d8ecd6b1898c/invoke.js"></script>
+<div id="container-f1bd6d29bbaabfc89aa7d8ecd6b1898c"></div>
 
         <a
           href="https://wa.me/923393301238"
